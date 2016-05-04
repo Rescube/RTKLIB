6cea8b4b96c9225b26edc843b3afc5f61120e844
Kopias Peter <kopias.peter@gmail.com>
2016-05-04 23:33:19 +0200

    odroid xu3 xu4 optimized makefiles

--- app/makeall-odroid.sh
+++ app/makeall-odroid.sh
@@ -2,30 +2,30 @@
 #
 # make all cui applications by gcc
 #
-EXTRAOPTION="-mcpu=cortex-a15 -mfpu=neon-vfpv4 -mfloat-abi=hard -ffast-math"
+EXTRAOPTION="-mtune=cortex-a15.cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -ffast-math"
 
 echo; echo % pos2kml/gcc
 cd pos2kml/gcc
-make $1 CTARGET="$EXTRAOPTION"
+make $1 EXTRA="$EXTRAOPTION" CC=gcc-5
 cd ../..
 
 echo; echo % str2str/gcc
 cd str2str/gcc
-make $1 CTARGET="$EXTRAOPTION"
+make $1 EXTRA="$EXTRAOPTION" CC=gcc-5
 cd ../..
 
 echo; echo % rnx2rtkp/gcc
 cd rnx2rtkp/gcc
-make $1 CTARGET="$EXTRAOPTION"
+make $1 EXTRA="$EXTRAOPTION" CC=gcc-5
 cd ../..
 
 echo; echo % convbin/gcc
 cd convbin/gcc
-make $1 CTARGET="$EXTRAOPTION"
+make $1 EXTRA="$EXTRAOPTION" CC=gcc-5
 cd ../..
 
 echo; echo % rtkrcv/gcc
 cd rtkrcv/gcc
-make $1 EXTRA="$EXTRAOPTION"
+make $1 EXTRA="$EXTRAOPTION" CC=gcc-5
 cd ../..
 
