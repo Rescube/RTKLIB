d2e7bbfc2b8b92fe0f19dbd895f39099ab0b493f
Kopias Peter <kopias.peter@gmail.com>
2016-06-04 19:11:53 +0100

    base_station.sh

--- bin/linux_x86_64/base_station.sh
+++ bin/linux_x86_64/base_station.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # it doesn't matter which ACM port we try, if we fail fast, we can just try the next one earlier
 DATE=`date +%Y%m%d_%H%M%S`
-./str2str -in serial://ttyACM0 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd 
-#./str2str -in serial://ttyACM1 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd -t 10
+./str2str -in serial://ttyACM0 -out tcpsvr://:2102 -out ~/rescube_audit/gpstrack/raw_$DATE.log
+
+#./str2str -in serial://ttyACM1 -out tcpsvr://:2102 -c 
 #./str2str -in serial://ttyACM2 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd -t 10
 #./str2str -in serial://ttyACM3 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd -t 10
