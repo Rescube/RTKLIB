02039a0b47399daa62a995ef74d9f8004203b426
MartonKrauter <krauter.marton@gmail.com>
2016-06-06 00:27:49 +0200

    Format change

--- bin/linux_x86_64/base_station.sh
+++ bin/linux_x86_64/base_station.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # it doesn't matter which ACM port we try, if we fail fast, we can just try the next one earlier
 DATE=`date +%Y%m%d_%H%M%S`
-./str2str -in serial://ttyACM0 -out tcpsvr://:2102 rtcm3 -out ~/rescube_audit/gpstrack/base_$DATE.rtcm3 -p 47.529909133 19.027932991 190.6687 -sta 100
+./str2str -in serial://ttyACM0#ubx -out tcpsvr://:2102#rtcm3 -out ~/rescube_audit/gpstrack/base_$DATE.rtcm3 -p 47.529909133 19.027932991 190.6687 -sta 100
 
 #./str2str -in serial://ttyACM1 -out tcpsvr://:2102 -c 
 #./str2str -in serial://ttyACM2 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd -t 10
