baa621dccfacd0dd78e139c90c09a07f4c72deb4
Kopias Peter <kopias.peter@gmail.com>
2016-07-02 10:03:03 +0930

    base station

---
+++ odroid_xu4_gcc5/base_stationACM1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# it doesn't matter which ACM port we try, if we fail fast, we can just try the next one earlier
+DATE=`date +%Y%m%d_%H%M%S`
+#./str2str -in serial://ttyACM0 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd -t 10
+./str2str -in serial://ttyACM1 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd -t 10
+#./str2str -in serial://ttyACM2 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd -t 10
+#./str2str -in serial://ttyACM3 -out tcpsvr://:2102 -c ~/rescube_audit/gpstrack/raw_$DATE.cmd -t 10
